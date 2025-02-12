9a38505ca022de2db04a53d8c01b1d26ac8f983c
jaser-Jass <yardley1@yandex.ru>
2025-02-12 17:41:39 +0300

    Подключили базу данных

--- websiterecipes/main/urls.py
+++ websiterecipes/main/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
- path('', views.index),
- path('about', views.about)
+ path('', views.index, name='home'),
+ path('about', views.about, name='about')
 ]
